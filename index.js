28f3a56da7d2520259c3fe9782866bf15f4085c4
ProfessorVij <133563290+vijayaratlakota@users.noreply.github.com>
2026-09-12 16:42:36 +0530

    update-2

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
  const express = require('express')
+ const { MongoClient } = require('mongodb')
+ const dotEnv = require('dotenv')
 
  const port = 5500
 
  const app = express()
 
- app.get('/apple', (req,res)=>{
-    res.send('Hey welcome top my website , apples are red in color ')
+ dotEnv.config()
+
+ MongoClient.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log('Connected to mongodb')
+    })
+    .catch((error)=>{
+        console.log('Error', error)
+    })
+
+ const oneMiddle = ((req,res,next)=>{
+    if(10<20){
+        next()
+    }
+ })
+
+ const twoMiddle = ((req,res,next)=>{
+    if(10<20){
+        next()
+    }
+ })
+
+ const threeMiddle = ((req,res,next)=>{
+    if(10<20){
+        next()
+    }
+ })
+
+ app.get('/home',oneMiddle, (req,res)=>{
+    res.send('Welcome to Homepage')
+ })
+
+ app.get('/about',twoMiddle,(req,res)=>{
+    res.send('Welcome to about page')
+ })
+
+ app.get('/user/:123',threeMiddle, (req,res)=>{
+    res.send('Hey 123, Welocme to our website')
  })
 
 
